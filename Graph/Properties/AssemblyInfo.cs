cafef3d41b1af718020c8cee13a0c4390c814cb9
Richard Ranft <vegassparkyrich@gmail.com>
2021-11-03 10:24:54 -0700

    Update projects/solution to VS2019 and target .NET 4.8

--- Graph/Properties/AssemblyInfo.cs
+++ Graph/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Graph")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Graph node UI components.  Branched from https://github.com/LogicalError/Graph")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Richard Ranft")]
 [assembly: AssemblyProduct("Graph")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Richard Ranft 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.17")]
-[assembly: AssemblyFileVersion("1.0.0.17")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
